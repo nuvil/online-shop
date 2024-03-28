2c5e45474d4956e94997cb08bfdaa8387576f613
Pavel Surovcev <surovcev.p@gmail.com>
2024-03-28 19:31:08 +0300

    add main app

---
+++ app/main/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from goods.models import Categories
+
+
+# Create your views here.
+def index(request):
+
+    
+
+    context = {
+        'title': "Home - Главная",
+        'content': "Магазин мебели HOME",
+    }
+
+    return render(request, "main/index.html", context)
+
+
+def about(request):
+    context = {
+        'title': "Home - Главная",
+        'content': "О нас",
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар.",
+    }
+
+    return render(request, "main/about.html", context)
